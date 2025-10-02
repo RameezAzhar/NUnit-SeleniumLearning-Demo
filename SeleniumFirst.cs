bfd81fb38b42bffa4b1b135bda4f7b42923220d2
SHAIKH RAMEEZ AZHAR <95407421+RameezAzhar@users.noreply.github.com>
2025-10-02 16:10:20 +0500

    Add files via upload

---
+++ SeleniumFirst.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeliniumLearning
+{
+    public class SeleniumFirst
+    {
+        [SetUp]
+        public void StartBrowser()
+        {
+
+        }
+        [Test]
+        public void Test1()
+        {
+
+        }
+    }
+}
